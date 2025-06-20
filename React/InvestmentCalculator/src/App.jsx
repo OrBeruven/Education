c18feaa741597531887b91beb094d03736909f6b
OrBeruven <162804168+OrBeruven@users.noreply.github.com>
2025-06-20 15:25:40 +0300

    handle invalid and negetive inputs, save values as strings

--- React/InvestmentCalculator/src/App.jsx
+++ React/InvestmentCalculator/src/App.jsx
@@ -4,36 +4,45 @@ import UserInput from "./components/UserInput";
 import ResultTable from "./components/ResultTable";
 
 const DEFAULT_USER_INPUT = {
-  initial: 10000,
-  annual: 1200,
-  returnRate: 6,
-  duration: 10,
+  initial: "10000",
+  annual: '1200',
+  returnRate: "6",
+  duration: "10",
 };
 
 function App() {
   const [userInput, setUserInput] = useState(DEFAULT_USER_INPUT);
 
-  function handleInputChange(event) {
-    const id = event.target.id;
-    const newValue = event.target.value;
-
+  function handleInputChange(id, newValue) {
     setUserInput((prev) => {
       const updatedInput = { ...prev };
-      updatedInput[id] = Number(newValue);
+      updatedInput[id] = newValue;
       return updatedInput;
     });
   }
+
+  const isInputValid = Number(userInput.duration) >= 1;
+
+  console.log(isInputValid);
 
   return (
     <>
       <UserInput userInput={userInput} onChange={handleInputChange}></UserInput>
 
-      <ResultTable
-        initialInvestment={userInput.initial}
-        annualInvestment={userInput.annual}
-        expectedReturn={userInput.returnRate}
-        duration={userInput.duration}
-      ></ResultTable>
+      {!isInputValid && (
+        <p className="error">
+          Please enter a valid duration (whole number greater than 0).
+        </p>
+      )}
+
+      {isInputValid &&
+        <ResultTable
+          initialInvestment={Number(userInput.initial)}
+          annualInvestment={Number(userInput.annual)}
+          expectedReturn={Number(userInput.returnRate)}
+          duration={Number(userInput.duration)}
+        ></ResultTable>}
+      
     </>
   );
 }
--- React/InvestmentCalculator/src/components/UserInput.jsx
+++ React/InvestmentCalculator/src/components/UserInput.jsx
@@ -1,55 +1,50 @@
 export default function UserInput({ userInput, onChange }) {
-  const isDurationValid =
-    Number.isInteger(userInput.duration) && userInput.duration > 0;
-
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
-          <label htmlFor="initial">Initial Investment</label>
+          <label>Initial Investment</label>
           <input
             type="number"
             id="initial"
             value={userInput.initial}
-            onChange={onChange}
+            onChange={(event) => onChange(event.target.id, event.target.value)}
+            required
           />
         </p>
         <p>
-          <label htmlFor="annual">Annual Investment</label>
+          <label>Annual Investment</label>
           <input
             type="number"
             id="annual"
             value={userInput.annual}
-            onChange={onChange}
+            onChange={(event) => onChange(event.target.id, event.target.value)}
+            required
           />
         </p>
       </div>
       <div className="input-group">
         <p>
-          <label htmlFor="returnRate">Expected Return (%)</label>
+          <label>Expected Return (%)</label>
           <input
             type="number"
             id="returnRate"
             value={userInput.returnRate}
-            onChange={onChange}
+            onChange={(event) => onChange(event.target.id, event.target.value)}
+            required
           />
         </p>
         <p>
-          <label htmlFor="duration">Duration (Years)</label>
+          <label>Duration (Years)</label>
           <input
             type="number"
             id="duration"
             value={userInput.duration}
-            onChange={onChange}
+            onChange={(event) => onChange(event.target.id, event.target.value)}
+            required
           />
         </p>
       </div>
-
-      {!isDurationValid && (
-        <p className="error">
-          Please enter a valid duration (Greater than 0).
-        </p>
-      )}
     </section>
   );
 }
